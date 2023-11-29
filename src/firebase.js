19da58c4b922d9e696241453ef3931620ac02b9a
Niranjan Babu <72197108+Niranjan-Babu@users.noreply.github.com>
2023-11-29 06:41:23 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,7 @@ import 'firebase/compat/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDdfrqD1axCi722Dt66jySe4Vr_Yr6i8PI",
-  authDomain: "mymusic-df1c1.firebaseapp.com",
-  projectId: "mymusic-df1c1",
-  storageBucket: "mymusic-df1c1.appspot.com",
-  messagingSenderId: "389789019237",
-  appId: "1:389789019237:web:7c00b70d6f8b0f5904aa8c",
-  measurementId: "G-98VPCJJG9N"
+  
 };
 
 const app = initializeApp(firebaseConfig);
